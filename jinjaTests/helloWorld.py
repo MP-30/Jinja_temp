e18cb4020e8cd11a234f3dc953927540ab20b26d
MP-30 <adityabhadauriya93@gmail.com>
2024-01-19 17:35:06 +0530

    basics

---
+++ jinjaTests/helloWorld.py
@@ -0,0 +1,5 @@
+from jinja2 import Environment, FileSystemLoader
+env = Environment(loader = FileSystemLoader('templates'))
+template = env.get_template('helloName.jinja')
+output = template.render(name = 'Feeks')
+print(output)
